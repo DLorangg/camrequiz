beaefe6ac0238e500037ed15cbec2951131c41f2
DLorangg <lorang.damian.epet20@gmail.com>
2026-04-25 16:32:34 -0300

    Eliminando pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
-import withSerwistInit from "@serwist/next";
-
-const withSerwist = withSerwistInit({
-  swSrc: "app/sw.ts",
-  swDest: "public/sw.js",
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Aquí puedes mantener cualquier otra configuración de Next.js que ya tuvieras
 };
 
-export default withSerwist(nextConfig);
+export default nextConfig;
